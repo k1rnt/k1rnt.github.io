c5f5ac0f704acd9fe975145f516730352226805f
k1rnt <eru08tera15mora@gmail.com>
2025-04-24 01:26:40 +0900

    add icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import astroIcon from "astro-icon";
 
 export default defineConfig({
   site: "https://k1rnt.me",
   outDir: "dist",
-  integrations: [tailwind()],
+  integrations: [tailwind(), astroIcon()],
 });
